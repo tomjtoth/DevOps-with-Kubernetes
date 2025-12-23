f8780a220c4b7cb745a8285c1c55760cb6dc52b9
Tamás Tóth <tomjtoth@gmail.com>
2025-12-23 10:52:45 +0200

    reading file_pong in log_server

--- log_server/src/main.rs
+++ log_server/src/main.rs
@@ -10,11 +10,13 @@ use axum::{Router, extract::State, routing::get};
 #[derive(Clone)]
 struct AppState {
     log_file: Arc<File>,
+    pong_file: Arc<File>,
 }
 
 #[tokio::main]
 async fn main() {
     let log_path = env::var("LOG_PATH").unwrap_or(String::from("data/log"));
+    let pong_path = env::var("PONG_PATH").unwrap_or(String::from("data/pong"));
 
     let app_state = AppState {
         log_file: Arc::new(
@@ -23,18 +25,32 @@ async fn main() {
                 .open(&log_path)
                 .expect(&format!(r#"unable to open LOG_PATH="{log_path}""#)),
         ),
+
+        pong_file: Arc::new(
+            File::options()
+                .read(true)
+                .open(&pong_path)
+                .expect(&format!(r#"unable to open PONG_PATH="{pong_path}""#)),
+        ),
     };
 
     let app = Router::new()
         .route(
             "/",
             get(
-                |State(AppState { mut log_file }): State<AppState>| async move {
-                    let mut contents = String::new();
+                |State(AppState {
+                     mut log_file,
+                     mut pong_file,
+                 }): State<AppState>| async move {
+                    let mut log_contents = String::new();
                     let _ = log_file.seek(SeekFrom::Start(0));
-                    let _ = log_file.read_to_string(&mut contents);
+                    let _ = log_file.read_to_string(&mut log_contents);
 
-                    contents
+                    let mut pong_contents = String::new();
+                    let _ = pong_file.seek(SeekFrom::Start(0));
+                    let _ = pong_file.read_to_string(&mut pong_contents);
+
+                    format!("{}\nPing / Pongs: {}", log_contents, pong_contents)
                 },
             ),
         )
