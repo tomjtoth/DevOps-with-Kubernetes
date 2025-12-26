ba3e10416a456b0dfc1a6cea34ae577855331e29
Tamás Tóth <tomjtoth@gmail.com>
2025-12-26 14:42:17 +0200

    added controls to landing page

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -39,11 +39,21 @@ async fn check_on_image() -> Result<()> {
 #[component]
 pub fn App() -> Element {
     use_server_future(check_on_image).unwrap();
+    let mut value = use_signal(|| String::new());
 
     rsx! {
         document::Stylesheet { href: asset!("/assets/tailwind.css") }
         h1 { "The project App" }
         img { src: "/10min-image", class: "max-w-100" }
+        form { onsubmit: |ev| ev.prevent_default(),
+            input { value, onchange: move |ev| value.set(ev.value()) }
+            button { "Create todo" }
+        }
+        ul {
+            li { "Learn Rust" }
+            li { "Learn Dioxus" }
+            li { "Build something" }
+        }
         h3 { "DevOps with Kubernetes 2025" }
     }
 }
