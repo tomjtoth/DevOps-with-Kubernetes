7ae2e855ff3151e585520e9c0a47ba745eb922e0
Tamás Tóth <tomjtoth@gmail.com>
2026-01-13 10:40:58 +0200

    pingpong to store pings into DB

--- pingpong/src/main.rs
+++ pingpong/src/main.rs
@@ -1,31 +1,37 @@
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use std::{
     env,
-    sync::{
-        Arc,
-        atomic::{AtomicU8, Ordering},
-    },
+    sync::{Arc, LazyLock},
 };
 
 use axum::{Router, extract::State, response::IntoResponse, routing::get};
 
+static DATABASE_URL: LazyLock<String> =
+    LazyLock::new(|| env::var("DATABASE_URL").expect("missing env var DATABASE_URL"));
+
 struct AppState {
-    counter: AtomicU8,
+    pool: Pool<Postgres>,
 }
 
 #[tokio::main]
 async fn main() {
-    let state = Arc::new(AppState {
-        counter: AtomicU8::new(0),
-    });
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&*DATABASE_URL)
+        .await
+        .expect(&format!("unable to connect to DB @ {}", &*DATABASE_URL));
+
+    let state = Arc::new(AppState { pool });
 
     let app = Router::new()
         .route("/pingpong", get(handle_browser))
         .route("/pings", get(handle_ping))
         .with_state(state);
 
-    let port = env::var("PORT").unwrap_or(String::from("3000"));
+    let ip = env::var("IP").expect("missing env var IP");
+    let port = env::var("PORT").expect("missing env var PORT");
 
-    let addr = format!("0.0.0.0:{}", &port);
+    let addr = format!("{}:{}", &ip, &port);
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
     println!("listening at http://{}/pingpong", &addr);
@@ -33,11 +39,26 @@ async fn main() {
 }
 
 async fn handle_ping(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    axum::response::Json::from(state.counter.load(Ordering::SeqCst))
+    let count: (i64,) = sqlx::query_as("SELECT count(*) FROM pings;")
+        .fetch_one(&state.pool)
+        .await
+        .expect("query failed");
+
+    axum::response::Json::from(count.0)
 }
 
 async fn handle_browser(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let current = state.counter.fetch_add(1, Ordering::SeqCst);
+    let count: (i64,) = sqlx::query_as(
+        "
+            WITH ins AS (
+                INSERT INTO pings DEFAULT VALUES
+            )
+            SELECT count(*) FROM pings;
+        ",
+    )
+    .fetch_one(&state.pool)
+    .await
+    .expect("query failed");
 
-    format!("pong {}", current)
+    format!("pong {}", count.0)
 }
